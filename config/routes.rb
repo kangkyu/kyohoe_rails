1dce88ba84405cada81968cece6800ea0937cd24
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-12-15 00:11:41 -0800

    migrate in doorkeeper tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  use_doorkeeper
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :postings, only: [:new, :create, :show]
   resources :users

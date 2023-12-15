669941a3a34b9fd67aae9fcf4745686c16197274
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-12-15 00:39:32 -0800

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :postings, only: [:new, :create, :show]
   resources :users
-  resources :user_session
+  resource :user_session, only: [:new, :create, :destroy]
 
-  get "sign_in", to: "user_session#new"
+  get "sign_in", to: "user_sessions#new"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.

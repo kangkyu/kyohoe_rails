e91684838b4e83a4d80e8515a690923c013ec1f6
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-12-14 19:40:23 -0800

    Open a way to add a new posting

---
+++ app/controllers/postings_controller.rb
@@ -0,0 +1,24 @@
+class PostingsController < ApplicationController
+  def new
+    @posting = Posting.new
+  end
+
+  def create
+    @posting = Posting.new(posting_params)
+    if @posting.save
+      redirect_to @posting
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @posting = Posting.find(params[:id])
+  end
+
+  private
+
+  def posting_params
+    params.require(:posting).permit(:title, :body, :video_url)
+  end
+end
